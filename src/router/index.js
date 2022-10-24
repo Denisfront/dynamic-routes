f7c45fb97f0889f232ab8884852c0533ef13c2e0
Denis <smaylik911@gmail.com>
2022-10-24 13:35:22 +0700

    feat: add dynamic for alias

--- src/router/index.js
+++ src/router/index.js
@@ -4,31 +4,55 @@ import HelloWorld from "../views/Articles.vue";
 import ArticleId from "../views/ArticleId.vue";
 import store from "@/store";
 
-const routes = [
+const translations = ["en", "ru"];
+
+let routes = [
   {
     path: "/",
     component: HomeView,
     name: "home",
-    alias: ["/ru", "/en"],
     children: [],
   },
   {
     path: "/article",
     name: "article",
     component: HelloWorld,
-    alias: ["/ru/article", "/en/article"],
   },
   {
     path: "/article/:id",
     component: ArticleId,
     name: "articleId",
     props: (route) => ({ id: route.params.id }),
-    alias: ["/ru/article/:id", "/en/article/:id"],
     children: [],
   },
 ];
 
-const translations = ["en", "ru", "gt"];
+function glueLanguageWithUrl(lang, url) {
+  return `/${lang}${url}`;
+}
+
+function addAliasForRoutes(routes) {
+  return routes.map((route) => {
+    const newAlias = translations.map((lang) => {
+      return glueLanguageWithUrl(lang, route.path);
+    });
+
+    let alias;
+
+    if (route.alias) {
+      alias = [...route.alias, ...newAlias];
+    } else {
+      alias = [...newAlias];
+    }
+
+    return {
+      ...route,
+      alias,
+    };
+  });
+}
+
+routes = addAliasForRoutes(routes);
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
@@ -36,14 +60,12 @@ const router = createRouter({
 });
 
 router.beforeEach((to, from, next) => {
-  console.log(to);
-
   const { lang } = store.state;
   const { path } = to;
   const urlLang = to.path.split("/")[1];
 
   if (!translations.includes(urlLang)) {
-    next({ path: `/${lang}${path}` });
+    next({ path: glueLanguageWithUrl(lang, path) });
   } else {
     if (urlLang !== lang) {
       store.dispatch("setLang", urlLang);
